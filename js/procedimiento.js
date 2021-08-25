46734e30f12638e8fdc6b2767d197b9119e676ef
jml74262 <78316332+jml74262@users.noreply.github.com>
2021-08-24 20:48:42 -0500

    Add files via upload

---
+++ js/procedimiento.js
@@ -0,0 +1,9 @@
+var a = 1;
+var b = 4;
+var c = 3;
+
+var op1=((-b)+Math.sqrt((b*b)-(4*a*c)))/(2*a);
+var op2=((-b)-Math.sqrt((b*b)-(4*a*c)))/(2*a);
+
+console.log("x1 =", op1);
+console.log("x2 =", op2);
